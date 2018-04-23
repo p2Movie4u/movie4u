eaf33fc10d291cc9a4cdcfcaf9a21506250089cf
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-04-23 13:30:55 +0200

    User data create

---
+++ bin/user_data.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    username: "Luis",
+    password: "Luis",
+    email: "luis@luis.com",
+    photo
+  },
+  {
+    username: "Mapi",
+    password: "Mapi",
+    email: "mapi@mapi.com",
+    photo
+  }
+];
