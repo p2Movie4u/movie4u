01ccd35a5538d54421b68be48a68430204e3d34e
luisnueda <nueda.luis@gmail.com>
2018-04-25 14:05:11 +0200

    imgprofile in commons solved

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const passport = require('passport');
 const authRoutes = express.Router();
 const User = require("../models/User");
 const uploadCloud = require('../config/cloudinary.js');
+const commons = require("../config/commons");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
@@ -28,13 +29,14 @@ authRoutes.post("/signup", uploadCloud.single("photo"), (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
   const email = req.body.email;
-  console.log(req.file)
+  
   if (username === "" || password === "") {
     res.render("auth/signup", { message: "Indicate username and password" });
     return;
   }
-
+  
   User.findOne({ username }, "username", (err, user) => {
+
     if (user !== null) {
       res.render("auth/signup", { message: "The username already exists" });
       return;
@@ -44,12 +46,12 @@ authRoutes.post("/signup", uploadCloud.single("photo"), (req, res, next) => {
     const hashPass = bcrypt.hashSync(password, salt);
 
     const newUser = new User({
-      username,
-      password: hashPass,
-      email,
-      photo: req.file.url
-    });
-
+        username,
+        password: hashPass,
+        email,
+        photo: commons.imgProfile(req.file)
+      });
+    
     newUser.save((err) => {
       if (err) {
         res.render("auth/signup", { message: "Something went wrong" });
